1c5d402e0d227da07eab34725eabcbfdeed8b014
Blake Im <blake.im@lyka.com.au>
2024-02-01 13:10:48 +0000

    documented the macro at a high-level

--- macros/rolling_average_generalised.sql
+++ macros/rolling_average_generalised.sql
@@ -1,7 +1,7 @@
-{% macro rolling_average_generalised(column_name, partition_by, order_by='created_at', rolling_average_days=7) %}
+{% macro rolling_average_generalised(column_name, partition_by, order_by='created_at', rolling_average_period=7) %}
     avg( {{ column_name }} ) OVER (
                 PARTITION BY {{ partition_by }}
                 ORDER BY {{ order_by }}
-                ROWS BETWEEN {{ rolling_average_days - 1 }} PRECEDING AND CURRENT ROW
-            ) AS avg_{{ rolling_average_days }}_periods_{{ column_name }}
+                ROWS BETWEEN {{ rolling_average_period - 1 }} PRECEDING AND CURRENT ROW
+            ) AS avg_{{ rolling_average_period }}_periods_{{ column_name }}
 {% endmacro %}
